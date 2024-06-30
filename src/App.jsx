f865c1f98fd7f4237837c091a29c4bd5ee5a7db4
Justin <justinbansal@gmail.com>
2024-06-30 14:36:12 -0400

    Move state up to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,92 @@
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
 import Timer from "./Timer";
+import Modal from "./Modal";
 
 function App() {
+  const [permission, setPermission] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [time, setTime] = useState(5);
+  const [timer, setTimer] = useState('paused');
+  const [timerId, setTimerId] = useState(null);
+
+  useEffect(() => {
+    const userVisited = JSON.parse(localStorage.getItem('visited'));
+    const userPermission = JSON.parse(localStorage.getItem('permission'));
+
+    if (userVisited && userPermission) {
+      setShowModal(false);
+      setPermission(true);
+    } else {
+      setShowModal(true);
+      setPermission(false);
+    }
+  }, [])
+
+  function notifyUser() {
+    if (!permission) return;
+    new Notification('Timer finished! 🍅');
+  }
+
+  function handleStart() {
+    if (timer === 'running' || time === 0) return;
+
+    if (timerId === null) {
+      const intervalId = setInterval(() => {
+        setTime(time => {
+          if (time > 0) {
+            return time - 1;
+          } else {
+            handlePause(intervalId);
+            notifyUser();
+            return time;
+          }
+        })
+      }, 1000);
+      setTimerId(intervalId);
+      setTimer('running');
+    }
+  }
+
+  function handlePause(intervalId) {
+    const idToClear = intervalId || timerId;
+    clearInterval(idToClear);
+    setTimerId(null);
+    setTimer('paused');
+  }
+
+  function handleReset() {
+    setTimer('reset');
+    clearInterval(timerId);
+    setTimerId(null);
+    setTime(5);
+  }
+
+  function handleEnableNotifications() {
+    Notification.requestPermission().then(result => {
+      result === 'granted' ? setPermission(true) : setPermission(false);
+      localStorage.setItem('permission', result === 'granted' ? JSON.stringify(true) : localStorage.setItem('permission', JSON.stringify(false)));
+    });
+    localStorage.setItem('visited', JSON.stringify(true));
+    setShowModal(false);
+  }
+
+  function handleDisableNotifications() {
+    setPermission(false);
+    setShowModal(false);
+    localStorage.setItem('visited', JSON.stringify(true));
+    localStorage.setItem('permission', JSON.stringify(false));
+  }
+
   return (
     <>
       <h1>Pomodoro Timer 🍅</h1>
-      <Timer />
+      <Timer time={time} timer={timer} handlePause={handlePause} handleReset={handleReset} handleStart={handleStart}/>
+      {showModal ?
+        <Modal handleEnableNotifications={handleEnableNotifications} handleDisableNotifications={handleDisableNotifications}/>
+      : ''}
     </>
   )
 }
--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,65 +1,13 @@
-import { useState, useEffect } from "react";
-
 import TimeDisplay from "./TimeDisplay";
 import Button from "./Button";
 
-function Timer() {
-  const [time, setTime] = useState(5);
-  const [timer, setTimer] = useState('paused');
-  const [timerId, setTimerId] = useState(null);
-  const [permission, setPermission] = useState(false);
-
-  useEffect(() => {
-    Notification.requestPermission().then(result => {
-      result === 'granted' ? setPermission(true) : setPermission(false);
-    })
-  }, [permission])
-
-  function notifyUser() {
-    if (!permission) return;
-    new Notification('Timer finished! 🍅');
-  }
-
-  function handleStart() {
-    if (timer === 'running' || time === 0) return;
-
-    if (timerId === null) {
-      const intervalId = setInterval(() => {
-        setTime(time => {
-          if (time > 0) {
-            return time - 1;
-          } else {
-            handlePause(intervalId);
-            notifyUser();
-            return time;
-          }
-        })
-      }, 1000);
-      setTimerId(intervalId);
-      setTimer('running');
-    }
-  }
-
-  function handlePause(intervalId) {
-    const idToClear = intervalId || timerId;
-    clearInterval(idToClear);
-    setTimerId(null);
-    setTimer('paused');
-  }
-
-  function handleReset() {
-    setTimer('reset');
-    clearInterval(timerId);
-    setTimerId(null);
-    setTime(5);
-  }
-
+function Timer(props) {
   return (
     <div className="timer">
-      <TimeDisplay timeRemaining={time}/>
+      <TimeDisplay timeRemaining={props.time}/>
       <div className="controls">
-        {timer === 'paused' || timer === 'reset' ? <Button type="start" handleClick={handleStart} /> : <Button type="pause" handleClick={() => handlePause(null)} />}
-        <Button type="reset" handleClick={handleReset} />
+        {props.timer === 'paused' || props.timer === 'reset' ? <Button type="start" handleClick={props.handleStart} /> : <Button type="pause" handleClick={() => props.handlePause(null)} />}
+        <Button type="reset" handleClick={props.handleReset} />
       </div>
     </div>
   )
